7e1a570904ab2702c08dfa9274f3855c8f00fd03
Gilbert Omosa <omosagilbert@gmail.com>
2025-01-20 18:58:47 +0300

    add views
    - write a function view in views.py which renders the templates.
    - update urls.py file to link the views to a URL.

--- DjangoProject/urls.py
+++ DjangoProject/urls.py
@@ -21,5 +21,7 @@ from main import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('about/', views.about, name='about'),
+    path('services/', views.services, name='services'),
     path('admin/', admin.site.urls),
 ]
